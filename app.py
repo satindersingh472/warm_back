171ebbc5672d0cd9351a6cae81a45a793cbde286
satinder <satindersingh472@gmail.com>
2022-10-18 17:15:55 -0600

    changed_port

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_restaurant():
 if(dbcreds.production_mode == True):
     print('Running in production mode')
     import bjoern #type: ignore
-    bjoern.run(app,'0.0.0.0',5000)
+    bjoern.run(app,'0.0.0.0',5111)
 else:
     print('Running in TESTING MODE')
     from flask_cors import CORS
